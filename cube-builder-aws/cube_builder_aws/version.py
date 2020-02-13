66b66cd9ef22f8c912230b8a609dd37dd473c2a0
Beto Noronha <beto_noronha@live.com>
2020-02-13 11:52:11 -0300

    update year - Close #6

--- cube-builder-aws/cube_builder_aws/version.py
+++ cube-builder-aws/cube_builder_aws/version.py
@@ -1,7 +1,7 @@
 
 #
 # This file is part of Cube Builder AWS.
-# Copyright (C) 2019 INPE.
+# Copyright (C) 2019-2020 INPE.
 #
 # Cube Builder AWS is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
