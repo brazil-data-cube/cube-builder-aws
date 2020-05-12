704b29dda8e3c30a9dafb8e991b321b96a22cd51
Beto Noronha <beto_noronha@live.com>
2020-05-12 13:54:51 -0300

    remove OBT oauth

--- cube-builder-aws/app.py
+++ cube-builder-aws/app.py
@@ -17,7 +17,6 @@ from config import USER, PASSWORD, HOST, DBNAME
 
 from cube_builder_aws.business import CubeBusiness
 from cube_builder_aws.validators import validate
-from cube_builder_aws.utils.auth import require_oauth_scopes
 from cube_builder_aws.version import __version__
 
 app = Flask(__name__)
@@ -48,7 +47,6 @@ def status():
 
 
 @app.route("/create", methods=["POST"])
-@require_oauth_scopes(scope="cube_builder_aws:metadata:POST")
 def create():
     # validate params
     data, status = validate(request.json, 'create')
@@ -56,14 +54,10 @@ def create():
         return jsonify(json.dumps(data)), 400
 
     cubes, status = business.create_cube(data)
-    return jsonify(
-        message = 'Cube created',
-        cubes = cubes
-    ), status
+    return jsonify(cubes), status
 
 
 @app.route("/start", methods=["GET"])
-@require_oauth_scopes(scope="cube_builder_aws:process:POST")
 def start():
     # validate params
     data, status = validate(request.args.to_dict(), 'process')
@@ -75,7 +69,6 @@ def start():
 
 
 @app.route("/create-grs", methods=["POST"])
-@require_oauth_scopes(scope="cube_builder_aws:metadata:POST")
 def craete_grs():
     # validate params
     data, status = validate(request.json, 'grs')
@@ -87,7 +80,6 @@ def craete_grs():
 
 
 @app.route("/create-raster-size", methods=["POST"])
-@require_oauth_scopes(scope="cube_builder_aws:metadata:POST")
 def craete_raster_size():
     # validate params
     data, status = validate(request.json, 'raster_size')
@@ -99,7 +91,6 @@ def craete_raster_size():
 
 
 @app.route("/cube-status", methods=["GET"])
-@require_oauth_scopes(scope="cube_builder_aws:metadata:GET")
 def get_status():
     # validate params
     data, status = validate(request.args.to_dict(), 'status')
