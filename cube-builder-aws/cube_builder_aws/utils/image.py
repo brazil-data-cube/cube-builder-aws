3d91929c6e171ca04382ce916db16cd6672dc3ff
Beto Noronha <beto_noronha@live.com>
2020-05-26 09:01:37 -0300

    add header in files

--- cube-builder-aws/cube_builder_aws/utils/image.py
+++ cube-builder-aws/cube_builder_aws/utils/image.py
@@ -1,8 +1,8 @@
 #
-# This file is part of Python Module for Cube Builder.
+# This file is part of Python Module for Cube Builder AWS.
 # Copyright (C) 2019-2020 INPE.
 #
-# Cube Builder is free software; you can redistribute it and/or modify it
+# Cube Builder AWS is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
